7a894e10de542b8297f5709db509eeba15aa10ad
omBukkawar <om31104bukkawar@gmail.com>
2025-09-19 02:19:41 +0530

    Update App.jsx

--- LDS/src/App.jsx
+++ LDS/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home'
+import Compare from './Pages/comparativeAnalysis'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/compare' element={<Compare/>}/>
       </Routes>
     </div>
   )
